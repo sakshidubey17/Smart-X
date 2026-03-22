a4c1dc44989a25c19df7d7815995d2525d2e11f8
Sakshi Dubey <esakshya17@gmail.com>
2026-03-22 12:45:27 +0530

    1-update route files and path

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth.routes.js";
-import userRoutes from "./routes/user.Routes.js";
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookiesParser from "cookie-parser";
 import {v2 as cloudinary} from "cloudinary";
+import postRoutes from "./routes/post.route.js";
 
 dotenv.config();
 
