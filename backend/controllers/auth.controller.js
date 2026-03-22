ec6a2e01129f41d9f81eafd9cfed095e87cc7c17
Sakshi Dubey <esakshya17@gmail.com>
2026-03-23 00:40:43 +0530

    1- post and controllers added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-import User from "../models/User.model.js";
+import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import {generateTokenAndSetCookie} from "../lib/utils/generateToken.js";
  
--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,11 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cookiesParser from "cookie-parser";
 import {v2 as cloudinary} from "cloudinary";
-import postRoutes from "./routes/post.route.js";
+
 
 dotenv.config();
 
@@ -25,6 +26,7 @@ app.use(cookiesParser());
 
 app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
+app.use("/api/posts",postRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
